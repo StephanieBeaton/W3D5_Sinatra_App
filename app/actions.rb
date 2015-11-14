08a9efb7bf6e3580e162c04f398a0b352ee641f6
StephanieBeaton <stefanybeaton@gmail.com>
2015-11-14 00:58:14 +0000

    6 submitting a message Part B the POST action

--- app/actions.rb
+++ app/actions.rb
@@ -9,3 +9,12 @@ end
 get '/messages/new' do
   erb :'messages/new'
 end
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author:   params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
